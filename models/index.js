2a343dc74fbd28213961d4d0e74e8de6256ebe0d
Frank Taylor <frank@tahzoo.com>
2018-10-09 16:07:01 -0500

    updates file name

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 import Adjective from './adjective';
-import Conjunction from './conjunctions.js';
-import Pronoun from './pronouns.js';
+import Conjunction from './conjunction';
+import Pronoun from './pronoun';
 
 
 export default { Adjective, Conjunction, Pronoun };
