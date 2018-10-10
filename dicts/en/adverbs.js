cb01418b3c5f4813d1d6a0874a6916801e2b423c
Frank Taylor <frank@tahzoo.com>
2018-10-10 15:51:30 -0500

    adds adverbs

---
+++ dicts/en/adverbs.js
@@ -0,0 +1,455 @@
+/**
+ * http://www.yourdictionary.com/index.php/pdf/articles/140.100adverbs.pdf
+ * http://www.grammar.cl/Basic/Adverbs_Frequency.htm
+ * https://dictionary.cambridge.org/us/grammar/british-grammar/about-adjectives-and-adverbs/adverbs-types
+ */
+export default [
+    {
+        adverb: 'accidentally',
+        type: 'manner',
+    },
+    {
+        adverb: 'absolutely',
+        type: 'degree',
+    },
+    {
+        adverb: 'already',
+        type: 'time',
+    },
+    {
+        adverb: 'always',
+        type: 'time',
+    },
+    {
+        adverb: 'almost',
+        type: 'degree',
+    },
+    {
+        adverb: 'angrily',
+        type: 'manner',
+    },
+    {
+        adverb: 'anxiously',
+        type: 'manner',
+    },
+    {
+        adverb: 'awkwardly',
+        type: 'manner',
+    },
+    {
+        adverb: 'badly',
+        type: 'manner',
+    },
+    {
+        adverb: 'blindly',
+        type: 'manner',
+    },
+    {
+        adverb: 'boastfully',
+        type: 'manner',
+    },
+    {
+        adverb: 'boldly',
+        type: 'manner',
+    },
+    {
+        adverb: 'bravely',
+        type: 'manner',
+    },
+    {
+        adverb: 'brightly',
+        type: 'manner',
+    },
+    {
+        adverb: 'cheerfully',
+        type: 'manner',
+    },
+    {
+        adverb: 'completely',
+        type: 'degree',
+    },
+    {
+        adverb: 'coyly',
+        type: 'manner',
+    },
+    {
+        adverb: 'crazily',
+        type: 'manner',
+    },
+    {
+        adverb: 'defiantly',
+        type: 'manner',
+    },
+    {
+        adverb: 'deftly',
+        type: 'manner',
+    },
+    {
+        adverb: 'deliberately',
+        type: 'manner',
+    },
+    {
+        adverb: 'devotedly',
+        type: 'manner',
+    },
+    {
+        adverb: 'doubtfully',
+        type: 'manner',
+    },
+    {
+        adverb: 'dramatically',
+        type: 'manner',
+    },
+    {
+        adverb: 'dutifully',
+        type: 'manner',
+    },
+    {
+        adverb: 'eagerly',
+        type: 'manner',
+    },
+    {
+        adverb: 'elegantly',
+        type: 'manner',
+    },
+    {
+        adverb: 'enormously',
+        type: 'manner',
+    },
+    {
+        adverb: 'enough',
+        type: 'degree',
+    },
+    {
+        adverb: 'evenly',
+        type: 'manner',
+    },
+    {
+        adverb: 'eventually',
+        type: 'time',
+    },
+    {
+        adverb: 'exactly',
+        type: 'manner',
+    },
+    {
+        adverb: 'faithfully',
+        type: 'manner',
+    },
+    {
+        adverb: 'finally',
+        type: 'time',
+    },
+    {
+        adverb: 'foolishly',
+        type: 'manner',
+    },
+    {
+        adverb: 'fortunately',
+        type: 'manner',
+    },
+    {
+        adverb: 'frequently',
+        type: 'frequency',
+    },
+    {
+        adverb: 'generally',
+        type: 'frequency',
+    },
+    {
+        adverb: 'gleefully',
+        type: 'manner',
+    },
+    {
+        adverb: 'gracefully',
+        type: 'manner',
+    },
+    {
+        adverb: 'happily',
+        type: 'manner',
+    },
+    {
+        adverb: 'hastily',
+        type: 'manner',
+    },
+    {
+        adverb: 'honestly',
+        type: 'manner',
+    },
+    {
+        adverb: 'hopelessly',
+        type: 'manner',
+    },
+    {
+        adverb: 'hourly',
+        type: 'frequency',
+    },
+    {
+        adverb: 'however',
+        type: 'linking',
+    },
+    {
+        adverb: 'hungrily',
+        type: 'manner',
+    },
+    {
+        adverb: 'innocently',
+        type: 'manner',
+    },
+    {
+        adverb: 'inquisitively',
+        type: 'manner',
+    },
+    {
+        adverb: 'irritably',
+        type: 'manner',
+    },
+    {
+        adverb: 'jealously',
+        type: 'manner',
+    },
+    {
+        adverb: 'just',
+        type: 'focusing',
+    },
+    {
+        adverb: 'justly',
+        type: 'manner',
+    },
+    {
+        adverb: 'kindly',
+        type: 'manner',
+    },
+    {
+        adverb: 'lately',
+        type: 'time',
+    },
+    {
+        adverb: 'lazily',
+        type: 'manner',
+    },
+    {
+        adverb: 'loosely',
+        type: 'manner',
+    },
+    {
+        adverb: 'madly',
+        type: 'manner',
+    },
+    {
+        adverb: 'mainly',
+        type: 'focusing',
+    },
+    {
+        adverb: 'merrily',
+        type: 'manner',
+    },
+    {
+        adverb: 'mortally',
+        type: 'manner',
+    },
+    {
+        adverb: 'mysteriously',
+        type: 'manner',
+    },
+    {
+        adverb: 'nervously',
+        type: 'manner',
+    },
+    {
+        adverb: 'never',
+        type: 'frequency',
+    },
+    {
+        adverb: 'normally',
+        type: 'frequency',
+    },
+    {
+        adverb: 'now',
+        type: 'time',
+    },
+    {
+        adverb: 'obediently',
+        type: 'manner',
+    },
+    {
+        adverb: 'obnoxiously',
+        type: 'manner',
+    },
+    {
+        adverb: 'occasionally',
+        type: 'frequency',
+    },
+    {
+        adverb: 'often',
+        type: 'frequency',
+    },
+    {
+        adverb: 'only',
+        type: 'focusing',
+    },
+    {
+        adverb: 'particularly',
+        type: 'focusing',
+    },
+    {
+        adverb: 'perfectly',
+        type: 'degree',
+    },
+    {
+        adverb: 'politely',
+        type: 'manner',
+    },
+    {
+        adverb: 'poorly',
+        type: 'degree',
+    },
+    {
+        adverb: 'powerfully',
+        type: 'manner',
+    },
+    {
+        adverb: 'promptly',
+        type: 'manner',
+    },
+    {
+        adverb: 'quickly',
+        type: 'manner',
+    },
+    {
+        adverb: 'quite',
+        type: 'degree',
+    },
+    {
+        adverb: 'rapidly',
+        type: 'manner',
+    },
+    {
+        adverb: 'rarely',
+        type: 'frequency',
+    },
+    {
+        adverb: 'rather',
+        type: 'degree',
+    },
+    {
+        adverb: 'regularly',
+        type: 'frequency',
+    },
+    {
+        adverb: 'rudely',
+        type: 'manner',
+    },
+    {
+        adverb: 'safely',
+        type: 'manner',
+    },
+    {
+        adverb: 'seldom',
+        type: 'frequency',
+    },
+    {
+        adverb: 'selfishly',
+        type: 'manner',
+    },
+    {
+        adverb: 'seriously',
+        type: 'manner',
+    },
+    {
+        adverb: 'shakily',
+        type: 'manner',
+    },
+    {
+        adverb: 'sharply',
+        type: 'manner',
+    },
+    {
+        adverb: 'silently',
+        type: 'manner',
+    },
+    { // one does not...
+        adverb: 'simply',
+        type: 'focusing',
+    },
+    {
+        adverb: 'slowly',
+        type: 'manner',
+    },
+    {
+        adverb: 'solemnly',
+        type: 'manner',
+    },
+    {
+        adverb: 'sometimes',
+        type: 'frequency',
+    },
+    {
+        adverb: 'speedily',
+        type: 'manner',
+    },
+    {
+        adverb: 'sternly',
+        type: 'manner',
+    },
+    {
+        adverb: 'still',
+        type: 'time',
+    },
+    {
+        adverb: 'technically',
+        type: 'manner',
+    },
+    {
+        adverb: 'tediously',
+        type: 'manner',
+    },
+    {
+        adverb: 'then',
+        type: 'linking',
+    },
+    {
+        adverb: 'too',
+        type: 'degree',
+    },
+    {
+        adverb: 'unexpectedly',
+        type: 'manner',
+    },
+    {
+        adverb: 'usually',
+        type: 'time',
+    },
+    {
+        adverb: 'very',
+        type: 'degree',
+    },
+    {
+        adverb: 'victoriously',
+        type: 'manner',
+    },
+    {
+        adverb: 'vivaciously',
+        type: 'manner',
+    },
+    {
+        adverb: 'warmly',
+        type: 'manner',
+    },
+    {
+        adverb: 'wearily',
+        type: 'manner',
+    },
+    {
+        adverb: 'weekly',
+        type: 'frequency',
+    },
+    {
+        adverb: 'wildly',
+        type: 'manner',
+    },
+    {
+        adverb: 'yearly',
+        type: 'frequency',
+    },
+];
--- dicts/en/index.js
+++ dicts/en/index.js
@@ -1,8 +1,9 @@
 import Adjectives from './adjectives.descriptive';
+import Adverbs from './adverbs';
 import Conjunctions from './conjunctions';
 import Prepositions from './prepositions';
 import Pronouns from './pronouns';
 
 export default {
-    Adjectives, Prepositions, Pronouns, Conjunctions,
+    Adjectives, Adverbs, Prepositions, Pronouns, Conjunctions,
 };
