a55876c966ab107f34851573e556151ad21fab03
Frank Taylor <frank@tahzoo.com>
2018-10-10 16:23:39 -0500

    adds interjections

--- dicts/en/index.js
+++ dicts/en/index.js
@@ -1,9 +1,10 @@
 import Adjectives from './adjectives.descriptive';
 import Adverbs from './adverbs';
 import Conjunctions from './conjunctions';
+import Interjections from './interjections';
 import Prepositions from './prepositions';
 import Pronouns from './pronouns';
 
 export default {
-    Adjectives, Adverbs, Prepositions, Pronouns, Conjunctions,
+    Adjectives, Adverbs, Prepositions, Pronouns, Conjunctions, Interjections,
 };
---
+++ dicts/en/interjections.js
@@ -0,0 +1,190 @@
+/**
+ * http://www.yourdictionary.com/index.php/pdf/articles/156.listof-interjections.pdf
+ */
+export default [
+    {
+        interjection: 'absolutely',
+        type: 'cognitive',
+        wordCategory: 'secondary',
+    },
+    {
+        interjection: 'ah',
+        type: 'emotive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'aha',
+        type: 'cognition',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'amen',
+        type: 'cognitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'blah',
+        type: 'emotive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'boo',
+        type: 'emotive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'cheers',
+        type: 'volitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'crud',
+        type: 'emotive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'crap',
+        type: 'emotive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'darn',
+        type: 'emotive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'dang',
+        type: 'emotive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'damn',
+        type: 'emotive',
+        wordCategory: 'secondary',
+    },
+    {
+        interjection: 'doh',
+        type: 'cognitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'd\'oh',
+        type: 'cognitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'duh',
+        type: 'cognitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'eh',
+        type: 'cognitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'goodness',
+        type: 'emotive',
+        wordCategory: 'secondary',
+    },
+    {
+        interjection: 'ha',
+        type: 'cognitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'haha',
+        type: 'cognitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'hey',
+        type: 'volitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'hi',
+        type: 'volitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'hmmm',
+        type: 'cognitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'huh',
+        type: 'cognitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'no',
+        type: 'volitive',
+        wordCategory: 'secondary',
+    },
+    {
+        interjection: 'nah',
+        type: 'volitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'nope',
+        type: 'volitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'oops',
+        type: 'cognitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'ouch',
+        type: 'emotive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'please',
+        type: 'volitive',
+        wordCategory: 'secondary',
+    },
+    {
+        interjection: 'shoot',
+        type: 'emotive',
+        wordCategory: 'secondary',
+    },
+    {
+        interjection: 'ugh',
+        type: 'emotive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'wat',
+        type: 'emotive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'wut',
+        type: 'cognitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'wow',
+        type: 'cognitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'yay',
+        type: 'emotive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'yup',
+        type: 'volitive',
+        wordCategory: 'primary',
+    },
+    {
+        interjection: 'yikes',
+        type: 'emotive',
+        wordCategory: 'primary',
+    },
+];
