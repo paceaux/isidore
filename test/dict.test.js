aa32c4e12ce395a14f60ce591c5b10ae29955365
Frank Taylor <frank@tahzoo.com>
2018-10-10 15:03:45 -0500

    adds tests for whole dictionary

--- test/dict.test.js
+++ test/dict.test.js
@@ -10,3 +10,62 @@ describe('you have dictionaries', () => {
         expect(Dictionaries).to.have.property('En');
     });
 });
+
+describe('you have stuff in the English dictionary', () => {
+
+    it('should have some descriptive adjectives', () => {
+        expect(Dictionaries).to.have.property('En');
+        const { En } = Dictionaries;
+
+        expect(En).to.have.property('Adjectives');
+    });
+
+    it('should have some adverbs', () => {
+        expect(Dictionaries).to.have.property('En');
+        const { En } = Dictionaries;
+
+        expect(En).to.have.property('Adverbs');
+    });
+
+    it('should have some conjunctions', () => {
+        expect(Dictionaries).to.have.property('En');
+        const { En } = Dictionaries;
+
+        expect(En).to.have.property('Conjunctions');
+    });
+
+    it('should have some interjections', () => {
+        expect(Dictionaries).to.have.property('En');
+        const { En } = Dictionaries;
+
+        expect(En).to.have.property('Interjections');
+    });
+
+    it('should have some nouns', () => {
+        expect(Dictionaries).to.have.property('En');
+        const { En } = Dictionaries;
+
+        expect(En).to.have.property('Nouns');
+    });
+
+    it('should have some prepositions', () => {
+        expect(Dictionaries).to.have.property('En');
+        const { En } = Dictionaries;
+
+        expect(En).to.have.property('Prepositions');
+    });
+
+    it('should have some pronouns', () => {
+        expect(Dictionaries).to.have.property('En');
+        const { En } = Dictionaries;
+
+        expect(En).to.have.property('Pronouns');
+    });
+
+    it('should have some verbs', () => {
+        expect(Dictionaries).to.have.property('En');
+        const { En } = Dictionaries;
+
+        expect(En).to.have.property('Verbs');
+    });
+});
