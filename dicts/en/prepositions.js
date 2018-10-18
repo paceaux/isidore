4537ecf45ca67838a487d0595396d29cb423093a
Frank Taylor <frank@tahzoo.com>
2018-10-17 21:38:13 -0500

    adds types

--- dicts/en/prepositions.js
+++ dicts/en/prepositions.js
@@ -2,52 +2,169 @@
 * English preposition list
 * https://www.talkenglish.com/vocabulary/top-50-prepositions.aspx
  */
+import PrepositionDictionary from '../../models/dictionaries/preposition.dictionary';
 
-export default [
-    'of',
-    'with',
-    'at',
-    'from',
-    'into',
-    'during',
-    'including',
-    'until',
-    'against',
-    'among',
-    'throughout',
-    'despite',
-    'towards',
-    'upon',
-    'concerning',
-    'to',
-    'in',
-    'for',
-    'on',
-    'by',
-    'about',
-    'like',
-    'through',
-    'over',
-    'before',
-    'between',
-    'after',
-    'since',
-    'without',
-    'under',
-    'within',
-    'along',
-    'following',
-    'across',
-    'behind',
-    'beyond',
-    'plus',
-    'except',
-    'but',
-    'up',
-    'out',
-    'around',
-    'down',
-    'off',
-    'above',
-    'near',
-];
+export default new PrepositionDictionary([
+    {
+        preposition: 'of',
+        type: 'origin',
+    },
+    {
+        preposition: 'with',
+        type: 'agency',
+    },
+    {
+        preposition: 'at',
+        type: 'time',
+    },
+    {
+        preposition: 'from',
+        type: 'origin',
+    },
+    {
+        preposition: 'into',
+        type: 'place',
+    },
+    {
+        preposition: 'during',
+        type: 'time',
+    },
+    {
+        preposition: 'including',
+    },
+    {
+        preposition: 'until',
+        type: 'time',
+    },
+    {
+        preposition: 'against',
+    },
+    {
+        preposition: 'among',
+    },
+    {
+        preposition: 'throughout',
+    },
+    {
+        preposition: 'despite',
+        type: 'connection',
+    },
+    {
+        preposition: 'towards',
+        type: 'direction',
+    },
+    {
+        preposition: 'upon',
+    },
+    {
+        preposition: 'concerning',
+    },
+    {
+        preposition: 'to',
+        type: 'direction',
+    },
+    {
+        preposition: 'in',
+        type: 'time',
+    },
+    {
+        preposition: 'for',
+        type: 'purpose',
+    },
+    {
+        preposition: 'on',
+        type: 'time,place',
+    },
+    {
+        preposition: 'by',
+        type: 'agency',
+    },
+    {
+        preposition: 'about',
+    },
+    {
+        preposition: 'like',
+    },
+    {
+        preposition: 'through',
+        type: 'place,purpose',
+    },
+    {
+        preposition: 'over',
+        type: 'place',
+    },
+    {
+        preposition: 'before',
+    },
+    {
+        preposition: 'between',
+        type: 'place',
+    },
+    {
+        preposition: 'after',
+    },
+    {
+        preposition: 'since',
+        type: 'time',
+    },
+    {
+        preposition: 'without',
+        type: 'instrument',
+    },
+    {
+        preposition: 'under',
+        type: 'place',
+    },
+    {
+        preposition: 'within',
+        type: 'place',
+    },
+    {
+        preposition: 'along',
+    },
+    {
+        preposition: 'following',
+    },
+    {
+        preposition: 'across',
+        type: 'place',
+    },
+    {
+        preposition: 'behind',
+        type: 'place',
+    },
+    {
+        preposition: 'beyond',
+    },
+    {
+        preposition: 'plus',
+    },
+    {
+        preposition: 'except',
+        type: 'connection',
+    },
+    {
+        preposition: 'up',
+    },
+    {
+        preposition: 'out',
+    },
+    {
+        preposition: 'around',
+        type: 'place',
+    },
+    {
+        preposition: 'down',
+    },
+    {
+        preposition: 'off',
+    },
+    {
+        preposition: 'above',
+        type: 'place',
+    },
+    {
+        preposition: 'near',
+        type: 'place',
+    },
+], 'En');
