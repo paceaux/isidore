c219d18af6854154b70b3e58eba28ff57331598f
Frank Taylor <frank@tahzoo.com>
2018-10-25 15:18:52 -0500

    adds a debug file

---
+++ test/debug.test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+
+const { expect } = chai;
+const grammar = require('..');
+
+const { Languages, Sentence } = grammar;
+const { En } = Languages;
+
+describe.skip('=====DEBUGGING =====', () => {
+
+});
