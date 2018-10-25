468e655efb5c3b18fd4f5604a68d50a64b089039
Frank Taylor <frank@tahzoo.com>
2018-10-25 10:19:31 -0500

    documents sentence and word

--- models/sentence.js
+++ models/sentence.js
@@ -36,7 +36,10 @@ function guessSentenceTypeByPunctuation(text) {
 
     return typeList[typeList.length - 1].type;
 }
-
+/**
+ * @param  {array} typeList array of objects that are the different sentence types
+ * @returns {array} array of strings that are the punctuations
+ */
 function getSentencePunctuations(typeList) {
     const results = [];
 
@@ -47,8 +50,11 @@ function getSentencePunctuations(typeList) {
     return results;
 }
 
+/** creates a regular expression from an array of punctuation
+ * @param  {array} punctuationList strings that contain punctuation
+ * @returns {RegExp} regular expression that can be used to identify punctuation
+ */
 function getPunctuationRegex(punctuationList) {
-
     const regexString = punctuationList.reduce((prev, cur, idx) => {
         const separator = idx ? '|' : '';
         const current = `(\\${cur})`;
@@ -59,6 +65,10 @@ function getPunctuationRegex(punctuationList) {
     return new RegExp(`(${regexString})`, 'g');
 }
 
+/** removes sentence-terminating/initiating punctuation
+ * @param  {string} text full string of text
+ * @returns {string} text without punctuation
+ */
 function getStrippedPunctuation(text) {
     const punctuationList = getSentencePunctuations(types);
     const puncuationRegex = getPunctuationRegex(punctuationList);
@@ -66,6 +76,10 @@ function getStrippedPunctuation(text) {
     return text.replace(puncuationRegex, '');
 }
 
+/** gets an array of lowercased words
+ * @param  {string} text full string of text (like a sentence)
+ * @returns {array} an array of strings that are all lowercased
+ */
 function getRawWordList(text) {
     const uncasedText = text.toLowerCase();
     const strippedPunctuation = getStrippedPunctuation(uncasedText);
@@ -73,14 +87,29 @@ function getRawWordList(text) {
 
     return strippedPunctuation.match(regex);
 }
-
+/**
+ * @param  {array} textArray array of words
+ * @param  {string} language two letter abbreviation for language
+ * @returns {array} array of either PartOfSpeech or Word objects
+ */
 function getWordList(textArray, language) {
     const dictionary = Dictionaries[language];
 
     return textArray.map((word) => dictionary.findWord(word)[0]);
 }
-
-function Sentence(text, language = 'En', type) {
+/**
+ * @param  {string} text a single sentence that contains terminating punctuation
+ * @param  {string} type type of sentence (will be guessed if not provided)
+ * @param  {} language='En' two-letter abbreviation for the language the sentence is in
+ * @member {string} text the original text of the sentence
+ * @member {string} language the language of the sentence (Default is English)
+ * @member {type} string the type of sentence (declarative, interrogative, imperative, exclamatory)
+ * @member {array} rawWordList only the words in the sentence
+ * @member {array} wordList each word in the sentence classified as either a part of speech or word
+ * @member {array} types the possible types that a sentence could have
+ * @method getSentenceType guesses the type of sentence based on punctuation
+ */
+function Sentence(text, type, language = 'En') {
     this.text = text;
     this.type = type;
     this.language = language;
--- models/word.js
+++ models/word.js
@@ -1,3 +1,9 @@
+/**
+ * @param  {String} word single word to be converted into a word object
+ * @member partOfSpeech=undefined
+ * @member type=undefined part of speech
+ * @member types the types that this word can have
+ */
 function Word(word) {
     this.partOfSpeech = undefined;
     this.word = word;
