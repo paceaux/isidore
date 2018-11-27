86f2ab27d58f23162453595eb758f37a3656c875
Frank Taylor <frank@tahzoo.com>
2018-11-27 07:59:22 -0600

    adds verb tenses to project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import PartsOfSpeech from '../models/partsOfSpeech/index';
 import Sentence from '../models/sentence';
 import Languages from '../dicts/index';
+import VerbTenses from '../models/verbtenses';
+import VerbTense from '../models/verbtense';
 
-export default { PartsOfSpeech, Sentence, Languages };
+export default {
+    PartsOfSpeech, Sentence, Languages, VerbTenses, VerbTense,
+};
