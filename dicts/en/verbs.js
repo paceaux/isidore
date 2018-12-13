316835c25e18c1d361f33b0b1ed60ed6cb78e0ce
Frank Taylor <frank@tahzoo.com>
2018-12-13 16:55:57 -0600

    adds regularverbs and sorts the trans/intrans verbs

--- dicts/en/verbs.js
+++ dicts/en/verbs.js
@@ -4,15 +4,26 @@
 import VerbDictionary from '../../models/dictionaries/verb.dictionary';
 import verbConjugations from './verbs.inflections';
 
+// https://www.linguasorb.com/english/verbs/regular-verbs/
+const regularVerbsList = ['accept', 'add', 'agree', 'allow', 'appear', 'argue', 'arrive', 'ask', 'base', 'believe', 'call', 'carry', 'cause', 'change', 'close', 'consider', 'continue', 'cover', 'create', 'decide', 'describe', 'determine', 'develop', 'die', 'drop', 'end', 'enter', 'expect', 'explain', 'face', 'fail', 'fill', 'focus', 'follow', 'happen', 'help', 'hope', 'identify', 'include', 'increase', 'indicate', 'involve', 'join', 'kill', 'lay', 'like', 'listen', 'live', 'look', 'love', 'move', 'need', 'note', 'occur', 'offer', 'open', 'pass', 'pick', 'place', 'plan', 'play', 'point', 'prepare', 'produce', 'protect', 'provide', 'pull', 'push', 'raise', 'reach', 'realize', 'receive', 'recognize', 'reduce', 'remain', 'remember', 'report', 'represent', 'require', 'return', 'save', 'seem', 'serve', 'share', 'start', 'stay', 'stop', 'suggest', 'support', 'talk', 'thank', 'try', 'turn', 'use', 'wait', 'walk', 'want', 'watch', 'wonder', 'work'];
+
 const transitiveVerbsList = [
-    'give', 'bring', 'buy', 'cost', 'give', 'get', 'leave', 'lend', 'make',
-    'offer', 'owe', 'pass', 'pay', 'play', 'promise', 'read', 'refuse', 'send', 'show',
-    'make', 'sing', 'sell', 'take', 'take', 'show', 'wish', 'help',
+    'bring', 'buy', 'cost', 'get', 'give', 'leave', 'lend', 'make', 'owe', 'pay', 'promise', 'read', 'refuse', 'sell', 'send', 'show', 'sing', 'take', 'wish',
+];
+// live, start,stop ===  is a regular verb
+const intransitiveVerbsList = [
+    'do', 'drive', 'live', 'run', 'set', 'start', 'stop', 'wash', 'write',
 ];
 
-const intransitiveVerbsList = [
-    'move', 'start', 'change', 'close', 'open', 'stop', 'do', 'set', 'run', 'live', 'wash', 'write', 'drive',
-];
+
+const regularVerbs = regularVerbsList.map(regularVerb => ({
+    verb: regularVerb,
+    type: 'transitive',
+    valence: {
+        arguments: 2,
+    },
+    conjugation: 'regular',
+}));
 
 const transitiveVerbs = transitiveVerbsList.map(commonNoun => ({
     verb: commonNoun,
@@ -31,6 +42,7 @@ const intransitiveVerbs = intransitiveVerbsList.map(commonNoun => ({
 }));
 
 export default new VerbDictionary([
+    ...regularVerbs,
     ...transitiveVerbs,
     ...intransitiveVerbs,
 ], 'En', verbConjugations);
