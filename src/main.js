9486bc44ce0753690f8cc40571d1bea5a6fbe69d
Frank Taylor <frank@tahzoo.com>
2018-12-12 19:30:08 -0600

    renames files to match classes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import PartsOfSpeech from '../models/partsOfSpeech/index';
 import Sentence from '../models/sentence';
 import Languages from '../dicts/index';
-import VerbConjugations from '../models/verbtenses';
-import VerbConjugation from '../models/verbtense';
+import VerbConjugations from '../models/verbconjugations';
+import VerbConjugation from '../models/verbconjugation';
 
 export default {
     PartsOfSpeech, Sentence, Languages, VerbConjugations, VerbConjugation,
