b81ab21d9e66685749066d81deaaa4a1d3cfed7b
Frank Taylor <frank@tahzoo.com>
2018-12-13 17:13:17 -0600

    adds an inflection for es

--- dicts/en/verbs.inflections.js
+++ dicts/en/verbs.inflections.js
@@ -11,9 +11,11 @@ const verbConjugations = new VerbConjugations(verbMoods, verbTenses, verbAspects
 // then, create inflections:
 const edInflection = { mutation: 'ed', quantity: 'singular+plural', person: '1+2+3' };
 const sInflection = { mutation: 's', quantity: 'singular', person: 3 };
+const esInflection = { mutation: 'es', quantity: 'singular', person: 3 };
 
 // add the inflections to the conjugations
 verbConjugations.addInflection('indicative:past:simple', edInflection);
 verbConjugations.addInflection('indicative:present:simple', sInflection);
+verbConjugations.addInflection('indicative:present:simple', esInflection);
 
 export default verbConjugations;
