64f13151721c052f9f51a5c49494eae2faad1705
Frank Taylor <frank.m.taylor08@gmail.com>
2019-06-28 12:01:41 -0500

    creates a constants file with language  dictionaries and coes

---
+++ constants.js
@@ -0,0 +1,36 @@
+const LANGUAGE_DICTIONARIES = new Map([
+    ['en', 'En'],
+    ['en-au', 'En'],
+    ['en-bz', 'En'],
+    ['en-ca', 'En'],
+    ['en-cb', 'En'],
+    ['en-gb', 'En'],
+    ['en-ie', 'En'],
+    ['en-jm', 'En'],
+    ['en-nz', 'En'],
+    ['en-ph', 'En'],
+    ['en-tt', 'En'],
+    ['en-us', 'En'],
+    ['en-za', 'En'],
+    ['en-zw', 'En'],
+]);
+
+const LANGUAGE_CODES = new Map([
+    ['en', 'English'],
+    ['en-BZ', 'English (Belize)'],
+    ['en-AU', 'English (Australia)'],
+    ['en-CA', 'English (Canada)'],
+    ['en-CB', 'English (Caribbean)'],
+    ['en-GB', 'English (United Kingdom)'],
+    ['en-IE', 'English (Ireland)'],
+    ['en-JM', 'English (Jamaica)'],
+    ['en-NZ', 'English (New Zealand)'],
+    ['en-PH', 'English (Republic of the Philippines)'],
+    ['en-TT', 'English (Trinidad and Tobago)'],
+    ['en-US', 'English (United States)'],
+    ['en-ZA', 'English (South Africa)'],
+    ['en-ZW', 'English (Zimbabwe)'],
+]);
+
+// eslint-disable-next-line import/prefer-default-export
+export { LANGUAGE_DICTIONARIES, LANGUAGE_CODES };
